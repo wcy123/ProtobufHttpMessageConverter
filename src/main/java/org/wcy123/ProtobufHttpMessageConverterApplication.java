7585aa77f32e653cf3f6dc0e806e1fd8a73260d5
Wang Chunye <wcy123@gmail.com>
2016-12-11 10:20:21 +0800

    Generate Project

---
+++ src/main/java/org/wcy123/ProtobufHttpMessageConverterApplication.java
@@ -0,0 +1,12 @@
+package org.wcy123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtobufHttpMessageConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtobufHttpMessageConverterApplication.class, args);
+	}
+}
